6f56cb366ef53b538f246ee810c97d413951ac14
Kyle Geske <stungeye@gmail.com>
2018-06-14 07:34:00 -0400

    Added 2011 HTML manitobaelection.ca site archive.

---
+++ 2011/manitoba-election-ca/html_archive/javascripts/application.js
@@ -0,0 +1,23 @@
+(function($){
+  $.fn.shuffle = function() {
+    return this.each(function(){
+      var items = $(this).children();
+      return (items.length)
+        ? $(this).html($.shuffle(items))
+        : this;
+    });
+  }
+
+  $.shuffle = function(arr) {
+    for(
+      var j, x, i = arr.length; i;
+      j = parseInt(Math.random() * i),
+      x = arr[--i], arr[i] = arr[j], arr[j] = x
+    );
+    return arr;
+  }
+})(jQuery);
+
+$(document).ready(function(){
+    $('div.politicians, div.parties, div.parties_thumb, ul.issues').shuffle();
+});
---
+++ 2011/manitoba-election-ca/html_archive/javascripts/map_highlight.js
@@ -0,0 +1,10 @@
+$(function() {
+        $('.highmap').maphilight( {   
+                stroke: true, 
+                strokeColor: '452B1A', 
+                strokeWidth: 3, 
+                strokeOpacity: 0.4, 
+                fillColor: 'aaffaa', 
+                fillOpacity: 0.20 } );
+
+    });
